132f3a395bde94b8d40fa324dc5150f86171fd35
Rail <rail01@protonmail.com>
2020-11-08 14:39:11 +0000

    Add localized namespace names

--- DPLforum.namespaces.php
+++ DPLforum.namespaces.php
@@ -22,8 +22,49 @@ $namespaceNames['en'] = array(
 	NS_FORUM_TALK => 'Forum_talk',
 );
 
+$namespaceNames['no'] = array(
+	NS_FORUM => 'Forum',
+	NS_FORUM_TALK => 'Forumdiskusjon',
+);
+
+$namespaceNames['es'] = array(
+	NS_FORUM => 'Foro',
+	NS_FORUM_TALK => 'Foro_Discusión',
+);
+
 /** Finnish (Suomi) */
 $namespaceNames['fi'] = array(
 	NS_FORUM => 'Foorumi',
 	NS_FORUM_TALK => 'Keskustelu_foorumista',
+);
+
+$namespaceNames['fa'] = array(
+	NS_FORUM => 'فوروم',
+	NS_FORUM_TALK => 'بحث_فوروم',
+);
+
+$namespaceNames['ja'] = array(
+	NS_FORUM => 'フォーラム',
+	NS_FORUM_TALK => 'フォーラム・トーク',
+);
+
+$namespaceNames['ko'] = array(
+	NS_FORUM => '포럼',
+	NS_FORUM_TALK => '포럼토론',
+);
+
+/** Polish (polski) */
+$namespaceNames['pl'] = array(
+	NS_FORUM => 'Forum',
+	NS_FORUM_TALK => 'Dyskusja_forum',
+);
+
+$namespaceNames['ru'] = array(
+	NS_FORUM => 'Форум',
+	NS_FORUM_TALK => 'Обсуждение_форума',
+);
+
+$namespaceNames['vi'] = array(
+	NS_FORUM => 'Diễn_đàn',
+	NS_FORUM_TALK => 'Thảo_luận_Diễn_đàn',
 );
